ba7fbc63573a6c04df0953c9b5dd05c5684f6cd7
Brian Hyams <bghyams@gmail.com>
2016-10-21 10:30:14 -0500

    Made small comment update requested by bghyams

--- convNeuralNet.py
+++ convNeuralNet.py
@@ -17,7 +17,7 @@ b = tf.Variable(tf.zeros([10]), name="b")
 y = tf.nn.softmax(tf.matmul(x, W) + b, name="softmax")
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-# Use gradient descent with learning rate of .5
+# We'll use gradient descent with learning rate of .5
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 #
