3bca53acc1e2cf583546b3a543d65b9f11a5ef99
Brian Hyams <bghyams@gmail.com>
2016-10-21 14:20:15 -0500

    Adding tensorflow tutorial's basic convolutional code and a project-wide .gitignore.
    Verified this works by running it.  Got an std::bad_alloc error when
    printing out results on test set.  Need to look into that later.
    
    This is by no means the final version.  Planning to continue changing
    the code.

--- convNeuralNet.py
+++ convNeuralNet.py
@@ -23,6 +23,10 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
 # Read in mnist data from official mnist source
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
@@ -38,25 +42,67 @@ b = tf.Variable(tf.zeros([10]), name="b")
 y = tf.nn.softmax(tf.matmul(x, W) + b, name="softmax")
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
+x_image = tf.reshape(x, [-1,28,28,1])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+for i in range(50):
+    batch = mnist.train.next_batch(50)
+    if i%10 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g"%(i, train_accuracy))
+    sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+
 # We'll use gradient descent with learning rate of .5
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 #
 # Alright, now it's time to train.
 # Let's init all variables, then get to training
 # pieces of the training set.
 #
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
+# sess = tf.Session()
+# sess.run(tf.initialize_all_variables())
 
-for i in range(1000):
-    print(i)
-    batch = mnist.train.next_batch(50)
-    dictFeed = {x: batch[0], y_:batch[1]}
+# for i in range(1000):
+#     print(i)
+#     batch = mnist.train.next_batch(50)
+#     dictFeed = {x: batch[0], y_:batch[1]}
 
-    sess.run(train_step, feed_dict=dictFeed)
+#     sess.run(train_step, feed_dict=dictFeed)
 
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    dictFeed = feed_dict={x: mnist.test.images, y_: mnist.test.labels}
-    print(sess.run(accuracy, feed_dict=dictFeed))
+#     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+#     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#     dictFeed = feed_dict={x: mnist.test.images, y_: mnist.test.labels}
+#     print(sess.run(accuracy, feed_dict=dictFeed))
