f49b9d9ecb413ff085cec9fe1f90ac151d459a5d
Brian Hyams <bghyams@gmail.com>
2016-10-29 19:24:52 -0500

    Doing a few whitespace fixups.

--- convNeuralNet.py
+++ convNeuralNet.py
@@ -44,7 +44,7 @@ y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_")
 W = tf.Variable(tf.zeros([784, 10]), name="W")
 b = tf.Variable(tf.zeros([10]), name="b")
 
-x_image = tf.reshape(x, [-1,28,28,1])
+x_image = tf.reshape(x, [-1, 28, 28, 1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
@@ -54,7 +54,7 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
+W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
 
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
@@ -70,7 +70,7 @@ y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
@@ -82,7 +82,7 @@ summary_writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
 
 for i in range(50):
     batch = mnist.train.next_batch(50)
-    if i%10 == 0:
+    if i % 10 == 0:
         train_accuracy = sess.run(accuracy, feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
     sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
