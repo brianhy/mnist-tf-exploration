1d0ebe9bc98009ff13016d24423848ce95b2595f
Brian Hyams <bghyams@gmail.com>
2016-11-16 08:50:43 -0600

    Transformed convolutional ReLU network into basic sigmoid (30 hidden neurons)
    Why are we doing this?  The purpose is to get back to a simpler neural
    network so that we can better grasp what's going on.  From there we can
    do things like add L2 regularization or better weight initialization to
    see how that affects things.
    
    1. Deleted all code related to convolutional network unless helpful for
    basic sigmoid.
    
    2. Add name param to weight_variable and bias_variable creation so
    tensorboard viz can be better annotated.
    
    3. Setup a single hidden layer sigmoid network with 30 hidden neurons.
    
    4. citemsInSet:  Wanted to make mini-batch set size the same.  When one
    is much larger than the other, the smaller set lacks granularity into
    probability which can make comparing train v. test more difficult (e.g.
    test set size of 10 and train set size of 100, we can have 10+
    probability differences pretty easily as it only takes one wrong
    labeling in set size of 10 to drop score to 90).  This consistency of
    set size isn't required if both sets are large-ish (say, 1000+), but for
    now while we're doing quick iterations for the sake of tuning over
    smaller batches, we'll keep this var in there.
    	Init'ing it to 100 because that didn't slow things down much.
    
    5. Reduced number of epochs to 100 because we were getting good progress
    in that span.  We needn't waste time with additional calcs.

--- convNeuralNet.py
+++ convNeuralNet.py
@@ -8,22 +8,14 @@ import tensorflow as tf
 # Create a tf weight variable with a small amount of noise.
 # Truncated normal is truncated at 2 std deviations.
 # Default mean is 0.0
-def weight_variable(shape):
+def weight_variable(shape, name=""):
     initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
+    return tf.Variable(initial, name)
 
 # Create a bias with slightly positive lean.
-def bias_variable(shape):
+def bias_variable(shape, name=""):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 def MsecNow():
     return int(round(time.time() * 1000))
@@ -40,48 +32,36 @@ logs_path = "/tmp/tensorflow_logs/mnistConvol"
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 #
-# Setup W*x + b = y and the expression to optimize:  cross-entropy
+# Setup placeholders for input layer and expected output
 #
 x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
 y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_")
 
-W = tf.Variable(tf.zeros([784, 10]), name="W")
-b = tf.Variable(tf.zeros([10]), name="b")
 
-# First convoluational vars
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+# Second Layer (first hidden layer)
+cNeuronsLyr2 = 30
+lyr2_W = weight_variable([784, cNeuronsLyr2], name="lyr2_W")
+lyr2_b = bias_variable([cNeuronsLyr2], name="lyr2_b")
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+lyr2_Activation = tf.nn.sigmoid(tf.matmul(x, lyr2_W) + lyr2_b)
 
-# Second convolutional vars
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+# Third Layer (Output Layer)
+lyr3_W = weight_variable([cNeuronsLyr2, 10], name="lyr3_W")
+lyr3_b = bias_variable([10], name="lyr3_b")
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+output = tf.nn.sigmoid(tf.matmul(lyr2_Activation, lyr3_W) + lyr3_b)
 
-W_fc1 = weight_variable([7*7*64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+# Cross-entropy calc might look a little odd because we're doing (1 - y_)... calc.
+# But, this is because cross-entropy must be calc'd over a probability distribution,
+# and raw sigmoid activation (which is what is in the "output" tensor) isn't a prob.
+# distribution.  Hence, we interpret each neuron as a Bernoulli distribution (0 or 1)
+# with output telling us probability of a single neuron in output is a 1.  Doing so
+# makes the cross-entropy optimization behave.
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(output) + (1 - y_) * tf.log(1 - output), reduction_indices=[1]))
 tf.scalar_summary("x-entr", cross_entropy)
 
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+train_step = tf.train.GradientDescentOptimizer(1).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 tf.scalar_summary("accuracy", accuracy)
@@ -97,15 +77,16 @@ train_summary_writer = tf.train.SummaryWriter(logs_path + "/train", graph=tf.get
 test_summary_writer = tf.train.SummaryWriter(logs_path + "/test", graph=tf.get_default_graph())
 
 # Setup test set
-iTestSetLim = min(10, len(mnist.test.images))
-dictTestData = {x: mnist.test.images[0:iTestSetLim], y_: mnist.test.labels[0:iTestSetLim], keep_prob: 1.0}
+citemsInSet = 100
+iTestSetLim = min(citemsInSet, len(mnist.test.images))
+dictTestData = {x: mnist.test.images[0:iTestSetLim], y_: mnist.test.labels[0:iTestSetLim]}
 
-for i in range(300):
-    batch = mnist.train.next_batch(10)
+for i in range(100):
+    batch = mnist.train.next_batch(citemsInSet)
     if ((i % 10) == 0):
         print("step %d"%(i))
-    sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-    train_summary = sess.run(mrg_summary, feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+    sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
+    train_summary = sess.run(mrg_summary, feed_dict={x: batch[0], y_: batch[1]})
     train_summary_writer.add_summary(train_summary, i)
     test_summary = sess.run(mrg_summary, feed_dict=dictTestData)
     test_summary_writer.add_summary(test_summary, i)
