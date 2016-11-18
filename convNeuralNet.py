1d049dc71131cf77541423265b3e9ea6a5b2d1d6
Brian Hyams <bghyams@gmail.com>
2016-11-18 15:33:08 -0600

    Turn sigmoid neural net script into a class.
    This looks like a big change, but it's essentially a big copy/paste of
    all the training code into the new Train method.
    
    This is all done to allow this file to be loaded as a module.

--- convNeuralNet.py
+++ convNeuralNet.py
@@ -21,6 +21,97 @@ def bias_variable(shape, name=""):
 def MsecNow():
     return int(round(time.time() * 1000))
 
+class SigmoidMnistNeuralNet(object):
+
+    def __init__(self,
+                    fltLrnRateIn=1.0,
+                    cEpochsIn=1000,
+                    citemsBatchIn=100,
+                    cNeuronsHiddenLryIn=30,
+                    fltL2RegParamIn=0.0,
+                    strLogFolderIn="/tmp/tensorflow_logs/mnistConvol"):
+        fltLrnRate = fltLrnRateIn
+        cEpochs = cEpochsIn
+        citemsBatch = citemsBatchIn
+        cNeuronsLyr2 = cNeuronsHiddenLryIn
+        fltL2RegParam = fltL2RegParamIn
+
+        # Here's where all the tensorflow logs will go.
+        # For example, things like graph viz and learning information
+        # will be dumped hereIn
+        strLogFolder = strLogFolderIn
+
+    def Train(self):
+        # Read in mnist data from official mnist source
+        mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+        #
+        # Setup placeholders for input layer and expected output
+        #
+        x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
+        y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_")
+
+
+        # Second Layer (first hidden layer)
+        lyr2_W = weight_variable([784, cNeuronsLyr2], name="lyr2_W")
+        lyr2_b = bias_variable([cNeuronsLyr2], name="lyr2_b")
+
+        lyr2_Activation = tf.nn.sigmoid(tf.matmul(x, lyr2_W) + lyr2_b)
+
+        # Third Layer (Output Layer)
+        lyr3_W = weight_variable([cNeuronsLyr2, 10], name="lyr3_W")
+        lyr3_b = bias_variable([10], name="lyr3_b")
+
+        output = tf.nn.sigmoid(tf.matmul(lyr2_Activation, lyr3_W) + lyr3_b)
+
+        # Cross-entropy calc might look a little odd because we're doing (1 - y_)... calc.
+        # But, this is because cross-entropy must be calc'd over a probability distribution,
+        # and raw sigmoid activation (which is what is in the "output" tensor) isn't a prob.
+        # distribution.  Hence, we interpret each neuron as a Bernoulli distribution (0 or 1)
+        # with output telling us probability of a single neuron in output is a 1.  Doing so
+        # makes the cross-entropy optimization behave.
+        cross_entropy = -tf.reduce_sum(y_ * tf.log(output) + (1 - y_) * tf.log(1 - output), reduction_indices=[1])
+
+        # We're going to add in some L2 regularization across the weights in our layers.
+        # This will be Lambda/N * Sum(w^2, across all weights) - where N is items in the
+        # mini-batch.
+        L2_reg = fltL2RegParam * (tf.nn.l2_loss(lyr2_W) + tf.nn.l2_loss(lyr3_W))
+        tf.scalar_summary("L2_reg", L2_reg)
+
+        cost = tf.reduce_mean(cross_entropy + L2_reg) # Here's where we divide cost by N (# items in mini-batch)
+        tf.scalar_summary("cost", cost)
+
+        train_step = tf.train.GradientDescentOptimizer(fltLrnRate).minimize(cost)
+        correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.scalar_summary("accuracy", accuracy)
+        #
+        # Now, it's time to create session and train things.
+        #
+        sess = tf.Session()
+        sess.run(tf.initialize_all_variables())
+
+        # Setup a summary writer so we can visualize the learning process
+        mrg_summary = tf.merge_all_summaries() # Get tensor that represents all summaries to make eval easier
+        train_summary_writer = tf.train.SummaryWriter(strLogFolder + "/train", graph=tf.get_default_graph())
+        test_summary_writer = tf.train.SummaryWriter(strLogFolder + "/test", graph=tf.get_default_graph())
+
+        # Setup test set
+        dictTestData = {x: mnist.test.images[0:citemsBatch], y_: mnist.test.labels[0:citemsBatch]}
+
+        for i in range(cEpochs):
+            batch = mnist.train.next_batch(citemsBatch)
+            if ((i % 10) == 0):
+                print("step {}".format(i))
+            sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
+            train_summary = sess.run(mrg_summary, feed_dict={x: batch[0], y_: batch[1]})
+            train_summary_writer.add_summary(train_summary, i)
+            test_summary = sess.run(mrg_summary, feed_dict=dictTestData)
+            test_summary_writer.add_summary(test_summary, i)
+
+        return sess.run(accuracy, feed_dict=dictTestData)
+
 def ParseCmdLine():
     parser = argp.ArgumentParser(description="Learn hand-written digits using Sigmoid network")
     parser.add_argument("-r", "--learningRate", default=1.0, type=float, dest="fltLrnRate",
@@ -52,75 +143,7 @@ fltL2RegParam = options.fltL2RegParam
 # will be dumped here
 strLogFolder = options.strLogFolder
 
-# Read in mnist data from official mnist source
-mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+smnn = SigmoidMnistNeuralNet(fltLrnRate, cEpochs, citemsBatch, cNeuronsLyr2, fltL2RegParam, strLogFolder)
+accuracy = smnn.Train()
 
-#
-# Setup placeholders for input layer and expected output
-#
-x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
-y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_")
-
-
-# Second Layer (first hidden layer)
-lyr2_W = weight_variable([784, cNeuronsLyr2], name="lyr2_W")
-lyr2_b = bias_variable([cNeuronsLyr2], name="lyr2_b")
-
-lyr2_Activation = tf.nn.sigmoid(tf.matmul(x, lyr2_W) + lyr2_b)
-
-# Third Layer (Output Layer)
-lyr3_W = weight_variable([cNeuronsLyr2, 10], name="lyr3_W")
-lyr3_b = bias_variable([10], name="lyr3_b")
-
-output = tf.nn.sigmoid(tf.matmul(lyr2_Activation, lyr3_W) + lyr3_b)
-
-# Cross-entropy calc might look a little odd because we're doing (1 - y_)... calc.
-# But, this is because cross-entropy must be calc'd over a probability distribution,
-# and raw sigmoid activation (which is what is in the "output" tensor) isn't a prob.
-# distribution.  Hence, we interpret each neuron as a Bernoulli distribution (0 or 1)
-# with output telling us probability of a single neuron in output is a 1.  Doing so
-# makes the cross-entropy optimization behave.
-cross_entropy = -tf.reduce_sum(y_ * tf.log(output) + (1 - y_) * tf.log(1 - output), reduction_indices=[1])
-
-# We're going to add in some L2 regularization across the weights in our layers.
-# This will be Lambda/N * Sum(w^2, across all weights) - where N is items in the
-# mini-batch.
-L2_reg = fltL2RegParam * (tf.nn.l2_loss(lyr2_W) + tf.nn.l2_loss(lyr3_W))
-tf.scalar_summary("L2_reg", L2_reg)
-
-cost = tf.reduce_mean(cross_entropy + L2_reg) # Here's where we divide cost by N (# items in mini-batch)
-tf.scalar_summary("cost", cost)
-
-train_step = tf.train.GradientDescentOptimizer(fltLrnRate).minimize(cost)
-correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_, 1))
-
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.scalar_summary("accuracy", accuracy)
-#
-# Now, it's time to create session and train things.
-#
-sess = tf.Session()
-sess.run(tf.initialize_all_variables())
-
-# Setup a summary writer so we can visualize the learning process
-mrg_summary = tf.merge_all_summaries() # Get tensor that represents all summaries to make eval easier
-train_summary_writer = tf.train.SummaryWriter(strLogFolder + "/train", graph=tf.get_default_graph())
-test_summary_writer = tf.train.SummaryWriter(strLogFolder + "/test", graph=tf.get_default_graph())
-
-# Setup test set
-iTestSetLim = min(citemsBatch, len(mnist.test.images))
-dictTestData = {x: mnist.test.images[0:iTestSetLim], y_: mnist.test.labels[0:iTestSetLim]}
-
-for i in range(cEpochs):
-    batch = mnist.train.next_batch(citemsBatch)
-    if ((i % 10) == 0):
-        print("step {}".format(i))
-    sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
-    train_summary = sess.run(mrg_summary, feed_dict={x: batch[0], y_: batch[1]})
-    train_summary_writer.add_summary(train_summary, i)
-    test_summary = sess.run(mrg_summary, feed_dict=dictTestData)
-    test_summary_writer.add_summary(test_summary, i)
-
-print("test accuracy - {0:.5f}".format(sess.run(accuracy, feed_dict=dictTestData)))
-
-print("Total Elapsed Time:  {0} msec".format(MsecNow() - msecStart))
+print("acc = {0:.5f}".format(accuracy))
